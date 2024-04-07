eaf15f2c08acd41e3cc741db016dad5a9157c0e9
Mansour Damanpak <50705400+cyanxxy@users.noreply.github.com>
2024-04-07 16:07:14 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import {
   Home,
   Feather,
-  Twitter,
+  Link,
   GitHub,
-  Figma,
+  Book,
   CheckSquare,
   Mic,
   Briefcase,
@@ -12,68 +12,16 @@ import {
 } from "react-feather";
 
 const config = {
-  app: {
-    title: "Reshaped",
-    subtitle: "Blog starter kit",
-    thumbnailUrl: "/img/logo.svg",
-  },
-  meta: {
-    url: "https://reshaped-blog-starter.vercel.app",
-    title: "Reshaped",
-    description: "Blog starter built on top of Next.js and Reshaped",
-    twitter: {
-      username: "blvdmitry",
-    },
-  },
+  // ...
   menu: [
-    {
-      icon: Home,
-      title: "Home",
-      href: "/",
-    },
-    {
-      icon: Feather,
-      title: "Writing",
-      href: "/article",
-    },
-    {
-      icon: Layers,
-      title: "Stack",
-      href: "/stack",
-    },
-
-    {
-      title: "Projects",
-      items: [
-        {
-          icon: Package,
-          title: "Reshaped",
-          href: "https://reshaped.so",
-        },
-        {
-          icon: CheckSquare,
-          title: "Design System Checklist",
-          href: "https://www.designsystemchecklist.com",
-        },
-        {
-          icon: Mic,
-          title: "Design System Interviews",
-          href: "https://reshaped.so/blog",
-        },
-        {
-          icon: Briefcase,
-          title: "Formaat Design",
-          href: "https://formaat.design",
-        },
-      ],
-    },
+    // ...
     {
       title: "Online",
       items: [
         {
-          icon: Twitter,
-          title: "Twitter",
-          href: "https://twitter.com/blvdmitry",
+          icon: Link,
+          title: "LinkedIn",
+          href: "https://www.linkedin.com/in/yourusername",
         },
         {
           icon: GitHub,
@@ -81,9 +29,9 @@ const config = {
           href: "https://github.com/formaat-design",
         },
         {
-          icon: Figma,
-          title: "Figma",
-          href: "https://www.figma.com/@reshaped",
+          icon: Book,
+          title: "Medium",
+          href: "https://medium.com/@yourusername",
         },
       ],
     },
