af30d16ae10cab3beef0d486faffaa93669d4e97
mansoor.damanpak <mansoor.damanpak@gmail.com>
2024-08-19 15:03:43 +0000

    fix again

--- src/components/LayoutSubmenu/LayoutSubmenu.tsx
+++ src/components/LayoutSubmenu/LayoutSubmenu.tsx
@@ -14,52 +14,47 @@ const LayoutSubmenu = (props: Props) => {
   if (!routeItems || routeItems.length <= 1) return null;
 
   return (
-    <>
-      <Hidden
-        hide={{
-          /**
-           * Hide on small viewports if we're on the article route
-           */
-          s: isArticle,
-          l: false,
-        }}
+    <Hidden
+      hide={{
+        /**
+         * Hide on small viewports if we're on the article route
+         */
+        s: isArticle,
+        l: false,
+      }}
+    >
+      <View
+        width={{ s: "100%", l: "320px", xl: "384px" }}
+        height="100%"
+        backgroundColor="elevation-base"
       >
-        {(className) => (
-          <View
-            width={{ s: "100%", l: "320px", xl: "384px" }}
-            height="100%"
-            backgroundColor="elevation-base"
-            className={className}
-          >
-            <ScrollArea scrollbarDisplay="hover">
-              <View padding={{ s: 4, l: 6 }} paddingBlock={3} gap={6}>
-                <View direction="row" gap={4} align="center">
-                  <Hidden hide={{ s: false, l: true }}>
-                    <LayoutMenuModal />
-                  </Hidden>
-                  <View.Item grow>
-                    <Text variant="body-3" weight="bold">
-                      {title}
-                    </Text>
-                  </View.Item>
-                </View>
+        <ScrollArea scrollbarDisplay="hover">
+          <View padding={{ s: 4, l: 6 }} paddingBlock={3} gap={6}>
+            <View direction="row" gap={4} align="center">
+              <Hidden hide={{ s: false, l: true }}>
+                <LayoutMenuModal />
+              </Hidden>
+              <View.Item grow>
+                <Text variant="body-3" weight="bold">
+                  {title}
+                </Text>
+              </View.Item>
+            </View>
 
-                <View gap={1}>
-                  {routeItems.map((item) => (
-                    <ArticleItem
-                      key={item.href}
-                      title={item.title}
-                      date={item.created}
-                      href={item.href}
-                    />
-                  ))}
-                </View>
-              </View>
-            </ScrollArea>
+            <View gap={1}>
+              {routeItems.map((item) => (
+                <ArticleItem
+                  key={item.href}
+                  title={item.title}
+                  date={item.created}
+                  href={item.href}
+                />
+              ))}
+            </View>
           </View>
-        )}
-      </Hidden>
-    </>
+        </ScrollArea>
+      </View>
+    </Hidden>
   );
 };
 
